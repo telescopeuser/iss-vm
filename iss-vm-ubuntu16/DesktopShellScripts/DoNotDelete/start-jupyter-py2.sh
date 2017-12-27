5d03efe668a39d7a223c1a82f653df07fd3297aa
Your Name <you@example.com>
2017-12-27 15:51:18 +0800

    iss-env-py3 -py2

--- iss-vm-ubuntu16/DesktopShellScripts/DoNotDelete/start-jupyter-py2.sh
+++ iss-vm-ubuntu16/DesktopShellScripts/DoNotDelete/start-jupyter-py2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-source /home/iss-user/anaconda3/envs/iss-env-py2/bin/activate iss-env-py2
+#source /home/iss-user/anaconda3/envs/iss-env-py2/bin/activate iss-env-py2
+source /home/iss-user/anaconda3/bin/activate iss-env-py2
 jupyter notebook
---
+++ iss-vm-ubuntu16/DesktopShellScripts/DoNotDelete/start-jupyter-py3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#source /home/iss-user/anaconda3/envs/iss-env-py3/bin/activate iss-env-py3
+source /home/iss-user/anaconda3/bin/activate iss-env-py3
+jupyter notebook
